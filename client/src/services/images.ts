6933eb7fe809a8e5a48293d7c6fbb85f03354a71
cds.dev003 <cds.dev003@gmail.com>
2025-01-15 23:45:56 +0700

    improve connect url path be

--- client/src/services/images.ts
+++ client/src/services/images.ts
@@ -3,7 +3,7 @@ import { ImageData,PaginationResult } from '@/types';
 export async function getImages (page:number = 1,pageSize:number = 10){
     let result:PaginationResult<ImageData>;
     try{
-        result = await fetch(`http://localhost:3000/api/images?page=${page}&pageSize=${pageSize}`)
+        result = await fetch(`https://gallery-slide-a2yo.vercel.app/api/images?page=${page}&pageSize=${pageSize}`)
          .then((res) => {
             if(res.ok && res.status === 200) return res.json()
          })
